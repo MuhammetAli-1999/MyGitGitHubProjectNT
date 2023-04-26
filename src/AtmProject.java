976ceb8f1ce2aee81e856c74fca01d5c34d1e9d9
Ali <muhammetalibegenc@gmail.com>
2023-04-26 22:01:47 +0300

    v3

--- src/AtmProject.java
+++ src/AtmProject.java
@@ -3,6 +3,10 @@ public class AtmProject {
 
         System.out.println("Bu bir atm projesidir");
 //version1
+        System.out.println("Java is easy");
+        //version2
+        System.out.println("learn java earn money ");
+        //version3
     }
 
 
